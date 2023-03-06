0a51a647b3ac0ba77cfcfd3983cddc0dccd72db9
Mohammed Rahman <mohammedrahman@Mohammeds-MacBook-Pro.local>
2023-03-06 18:59:43 +0000

    Day 1 Python

---
+++ week-01/day-01.py
@@ -0,0 +1,111 @@
+# Python is a scripting/programming language.
+
+# echo %PATH
+# Introduction to Python.
+
+# comments using #
+
+# stdout "Standard Out"
+
+# "Hello world!" - print("hello world") to the console/terminal the equivalent to console.log() in Javascript.
+
+# print("Hello World!")
+# print("This is Python programming printing")
+
+# Variables - a Container for Data
+
+# Creating/defining a variable
+
+# Basic data types
+# Integer
+# String
+# Boolean
+# Float
+
+# new_variable = "Value"
+
+# print(new_variable)
+
+# my_text = "This is my string variable"
+
+# print(my_text)
+
+# Rules.
+# We can't put spaces in variable names i.e first name = "John"
+# We can't start variable names with a number i.e 10something = "Animals"
+# We can't use "-" in a name also i.e full-name = "John Snow"
+
+# Conventions.
+# Give vairables meaningful names.
+# Bad variable name
+# asfasfsadf = "John Snow"
+# Good variable name
+# full_name = "John Snow"
+
+# input() input("prompt")
+# stdin: Standard In
+# input() - Will pause execution of code, and wait for input from the user.
+
+
+# print("Hello world")
+# print("More strings")
+# print("Even more printed strings")
+# The prompt Parameter allows us to promt the user for an input
+# input("Enter favourite Food\n")
+# len_check = len("Hello World")
+# print(len_check)
+
+# Press Enter to continue
+# input("Press Enter to continue\n")
+
+# Using Variables to store the value of input()
+
+# my_variable = input("I will store what ever is inputed here\n")
+
+# print(my_variable)
+
+# first_name = input("Enter First Name\n")
+# second_name = input("Enter Second Name\n")
+# String (Concatenation)
+# print(first_name + " " + second_name)
+# f string concatenation
+# print(f"{first_name} {second_name}")
+
+# Excercise 1
+# "Complete the code: Find the Sum of Variables x and y"
+# x = 100
+# y = 900
+# z = y + x
+# print(z)
+
+
+# Excercise 2
+# "Take input from the user and Print their string out 4 times back."
+# input_string = input("")
+
+
+# input_string = input('Enter your word:')
+# times = int(input('How many times would you like to repeat your word?'))
+# for i in range(times):
+#     print(input_string)
+
+# or
+# input_string = input('Enter your word:')
+# print(input_string)
+# print(input_string)
+# print(input_string)
+# print(input_string)
+
+# or
+# print("Hello\n" * 5)
+
+# Excercise 3
+# "Create 3 variables and take input: firstName, lastName, Age and let the user enter them."
+# first_name = input("Enter First Name\n")
+# last_name = input("Enter Second Name\n")
+# age = input("How old are you?\n")
+
+# Excercise 4
+# "Create and Display a Welcome Messge: Greet the user with their full name and tell them their age."
+
+# print(f"Hello {first_name} {last_name} you are {age} years old.")
